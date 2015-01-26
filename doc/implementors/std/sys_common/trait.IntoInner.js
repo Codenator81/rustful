c8bce9b84c3bed067d6510acaa35294909694ea5
Travis <travis@travis-ci.org>
2015-01-26 07:30:42 +0000

    Update docs from Travis build 118

--- doc/implementors/std/sys_common/trait.IntoInner.js
+++ doc/implementors/std/sys_common/trait.IntoInner.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustful'] = [];
+implementors['hyper'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
