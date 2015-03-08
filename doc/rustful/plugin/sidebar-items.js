e77e0c7f4846814ce6627be412ab37b63413b193
Travis <travis@travis-ci.org>
2015-03-08 09:31:27 +0000

    Update docs from Travis build 136

--- doc/rustful/plugin/sidebar-items.js
+++ doc/rustful/plugin/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ContextAction","The result from a context plugin."],["ResponseAction","The result from a `ResponsePlugin`."]],"trait":[["ContextPlugin","A trait for context plugins."],["ResponsePlugin","A trait for response plugins."]]});
+initSidebarItems({"trait":[["ContextPlugin","A trait for context plugins."],["ResponsePlugin","A trait for response plugins."]],"enum":[["ContextAction","The result from a context plugin."],["ResponseAction","The result from a `ResponsePlugin`."]]});
