21696d21015f21559a87da797eedf73a0f895fcd
Travis <travis@travis-ci.org>
2015-03-29 11:25:11 +0000

    Update docs from Travis build 141

--- doc/implementors/hyper/server/trait.Handler.js
+++ doc/implementors/hyper/server/trait.Handler.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, H, C&gt; HyperHandler for <a class='struct' href='rustful/server/struct.ServerInstance.html' title='rustful::server::ServerInstance'>ServerInstance</a>&lt;R, C&gt;",];
+implementors['rustful'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R, H, C&gt; <a class='trait' href='https://hyperium.github.io/hyper/hyper/index.html/hyper/server/trait.Handler.html' title='hyper::server::Handler'>HyperHandler</a> for <a class='struct' href='rustful/server/struct.ServerInstance.html' title='rustful::server::ServerInstance'>ServerInstance</a>&lt;R, C&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
