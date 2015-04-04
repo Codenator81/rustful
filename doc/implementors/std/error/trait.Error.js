03054b3dfc32dae6b0ed89df6db0f4095622cf1d
Travis <travis@travis-ci.org>
2015-04-04 09:01:47 +0000

    Update docs from Travis build 145

---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustful'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='rustful/enum.HttpError.html' title='rustful::HttpError'>HttpError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='rustful/response/enum.ResponseError.html' title='rustful::response::ResponseError'>ResponseError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
